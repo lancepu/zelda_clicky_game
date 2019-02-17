c95b31e5c88725e78fbe4f18ab951b387bf6998b
LanFP888 <pu.lance@gmail.com>
2019-02-16 21:24:40 -0500

    Updates

---
+++ precache-manifest.bcf1a1aed6c5912adb3dc0fa0c7763e8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b5fb5d97160996ca9b3ca40955308ba",
+    "url": "/zelda_clicky_game/static/media/kass.7b5fb5d9.png"
+  },
+  {
+    "revision": "a850b5394b14076f2acc",
+    "url": "/zelda_clicky_game/static/css/main.2ca233b7.chunk.css"
+  },
+  {
+    "revision": "cd120193f4af5598ae88",
+    "url": "/zelda_clicky_game/static/js/runtime~main.cd120193.js"
+  },
+  {
+    "revision": "8531d333ce09a69d32159c72fe4971fb",
+    "url": "/zelda_clicky_game/static/media/hyliaserifbeta-regular-webfont.8531d333.woff2"
+  },
+  {
+    "revision": "c741fa9d91f2dd500e04",
+    "url": "/zelda_clicky_game/static/js/2.c741fa9d.chunk.js"
+  },
+  {
+    "revision": "36af0a29939cb526c24bbd11248faf89",
+    "url": "/zelda_clicky_game/static/media/bokoblin.36af0a29.png"
+  },
+  {
+    "revision": "8813690b029eae9ce0f10cd61146482c",
+    "url": "/zelda_clicky_game/static/media/daruk.8813690b.png"
+  },
+  {
+    "revision": "3a6950eab645f7baacece2ce101c05d3",
+    "url": "/zelda_clicky_game/static/media/epona.3a6950ea.png"
+  },
+  {
+    "revision": "d3708fc7f26f495e51866bc5a6e031bc",
+    "url": "/zelda_clicky_game/static/media/gannon.d3708fc7.png"
+  },
+  {
+    "revision": "596cf77bdd8d4d1603ef87a21b0566c1",
+    "url": "/zelda_clicky_game/static/media/guardian.596cf77b.png"
+  },
+  {
+    "revision": "a850b5394b14076f2acc",
+    "url": "/zelda_clicky_game/static/js/main.a850b539.chunk.js"
+  },
+  {
+    "revision": "d95f92cde5fcc267e77a5d181ea02a04",
+    "url": "/zelda_clicky_game/static/media/link.d95f92cd.png"
+  },
+  {
+    "revision": "2c066db85ced2e0dcae5887c01b6f6f9",
+    "url": "/zelda_clicky_game/static/media/lynel.2c066db8.png"
+  },
+  {
+    "revision": "203ea1b3286ac7ff295efe1cf6ec58db",
+    "url": "/zelda_clicky_game/static/media/mipha.203ea1b3.png"
+  },
+  {
+    "revision": "5841047433f58a513595b06e1f27cc26",
+    "url": "/zelda_clicky_game/static/media/revali.58410474.png"
+  },
+  {
+    "revision": "75f77e4bb710600ac42c0f05d48b1c34",
+    "url": "/zelda_clicky_game/static/media/urbosa.75f77e4b.png"
+  },
+  {
+    "revision": "26b552fc7f798d414b2373421be2a39b",
+    "url": "/zelda_clicky_game/static/media/zelda.26b552fc.png"
+  },
+  {
+    "revision": "7ae50c8d9cf1173ad1579a7a613949f4",
+    "url": "/zelda_clicky_game/static/media/hyliaserifbeta-regular-webfont.7ae50c8d.woff"
+  },
+  {
+    "revision": "c741fa9d91f2dd500e04",
+    "url": "/zelda_clicky_game/static/css/2.d35a7924.chunk.css"
+  },
+  {
+    "revision": "d20b6e0a1ae4fee587744d2d3139212c",
+    "url": "/zelda_clicky_game/index.html"
+  }
+];
